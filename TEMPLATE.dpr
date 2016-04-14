0c069f28a77ccb0518ac472305eb9072386e24b2
luxidea <contact@luxidea.net>
2016-04-14 13:48:28 +0900

    begin

---
+++ TEMPLATE.dpr
@@ -0,0 +1,15 @@
+program TEMPLATE;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Main in 'Main.pas' {Form1},
+  Core in 'Core.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
